8679ad98da34e049a465aedfe971a3044559f35b
DYH414 <737760221@qq.com>
2026-05-24 10:07:47 +0800

    refactor(postgrad): 移除推荐历史和通用CRUD服务相关代码
    - 删除了AppRecommendationController及其历史记录查询功能
    - 移除了前端appRecommendation.js API接口文件
    - 删除了IPostgradCrudService通用CRUD服务接口
    - 移除了PostgradCrudMapper数据访问层接口
    - 清理了PostgradCrudServiceImpl服务实现类及其元数据配置
    - 移除了动态SQL构建和模块定义相关功能代码

--- ruoyi-ui/src/api/postgrad/appRecommendation.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/appRequest'
-
-export function history() {
-  return request({ url: '/app/recommendation/history', method: 'get' })
-}
-
-export function historyDetail(id) {
-  return request({ url: '/app/recommendation/history/' + id, method: 'get' })
-}
